7e4cf60f31cb558d3c6e92b4cb79f6a33e15c92c
noorrania <63913914+noorrania@users.noreply.github.com>
2021-07-27 22:56:33 +0600

    Update Greater_array_percentage.java

--- array elements and greater than average/src/Greater_array_percentage.java
+++ array elements and greater than average/src/Greater_array_percentage.java
@@ -2,30 +2,31 @@ import java.util.Scanner;
 
 public class Greater_array_percentage {
 
-	public static void main(String[] args) {
-		int sum;
-double avr,percentage,count=0;
+	public static void main(String[] args) 
+	{
 		Scanner in=new Scanner(System.in);
-		int[] arr=new int[6];
-for(int i=0;i<arr.length;i++)
-{
-	arr[i]=in.nextInt();
-	
-}
-sum=arr[0]+arr[1]+arr[2]+arr[3]+arr[4]+arr[5];
-avr=(double)sum/6;
-	
-	for(int data:arr) {
-	if(avr<data)
-		{count++;
-			
-		}}
-	 percentage=(count*100)/6;
+		int sum;
+		double avr,percentage,count=0;
+                int[] arr=new int[6];
+		for(int i=0;i<arr.length;i++)
+		{
+			arr[i]=in.nextInt();
 
-	System.out.printf("%.2f",percentage);
-	System.out.print("%");
-	
-	in.close();
+		}
+		sum=arr[0]+arr[1]+arr[2]+arr[3]+arr[4]+arr[5];
+		avr=(double)sum/6;
+
+		for(int data:arr) {
+			if(avr<data)
+			{count++;
+
+			}}
+		percentage=(count*100)/6;
+
+		System.out.printf("%.2f",percentage);
+		System.out.print("%");
+
+		in.close();
 	}
 
 }
